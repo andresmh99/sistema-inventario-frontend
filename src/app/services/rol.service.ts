15c67a1c0dc6b0bd5ca0117a7766ce60af0243c3
Jose Martinez <andresjose.210299@gmail.com>
2023-12-29 03:24:58 -0300

    URL

--- src/app/services/rol.service.ts
+++ src/app/services/rol.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { IRol } from '../interfaces/IRoles/roles';
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { IUsuario } from '../interfaces/IUsuarios/usuarios';
 
 @Injectable({
